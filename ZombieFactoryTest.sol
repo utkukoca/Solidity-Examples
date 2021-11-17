ef0eebef05c4e40fc691e86718f5a15f60298c54
utkukoca <49529559+utkukoca@users.noreply.github.com>
2021-11-17 17:13:38 +0300

    Update ZombieFactoryTest.sol

--- ZombieFactoryTest.sol
+++ ZombieFactoryTest.sol
@@ -12,10 +12,19 @@ contract ZombieFactory{
         uint dna;
         
     }
+    
+    mapping (uint=>address) public zombieToOwner; //bir zombinin hangi adrestte olduğunu belirlemek için kullanılır.
+    mapping (address =>uint) ownerZombieCount; //bir adrestte kaç tane zombi olduğunu belirlemek için kullanılır.
+    
     Zombie[] public zombies;
     function createZombie(string memory _name, uint _dna) private{
         zombies.push(Zombie(_name, _dna));
         uint id = zombies.length - 1; //dizideki ilk öğrenin indeksi 0 olduğundan -1 kullanmalıyız.
+        zombieToOwner[id] = msg.sender; //zombinin ıdsi altında kontratla iletişme geçen hesap tutulur.
+        ownerZombieCount[msg.sender]++;//bu adresin sahip olduğu zombi miktarını arttıralım.
+        
+        
+        
         emit NewZombie(id, _name, _dna);
     }
     function generateRandomDna(string memory _str) private view returns(uint){
@@ -24,6 +33,7 @@ contract ZombieFactory{
         
     }
     function createRandomZombie (string memory _name) public {
+        require (ownerZombieCount[msg.sender] == 0 ); //sadece hesabında hiç zombi bulunmayan hesaplar oluşturabilir yani her adres = 1 zombi.
         uint randDna = generateRandomDna(_name); //randDna adlı bir uint oluşturalım ve girilen ismi generateRandomDna adlı fonksiyonda çalıştıralım.
         createZombie(_name,randDna); //zombinin adını ve dnasını createZombie fonkisyonunu kullaranak dizinin içine push edelim.
     }
