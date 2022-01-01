c6075aad4fafbd705d5feb45dca2d3007358bbc5
utkukoca <49529559+utkukoca@users.noreply.github.com>
2022-01-01 23:30:17 +0300

    Update zombiefeedingtest.sol

--- zombiefeedingtest.sol
+++ zombiefeedingtest.sol
@@ -1,6 +1,5 @@
 pragma  solidity  ^0.8.4;
 
-
 import "./zombiefactory.sol";
 
 abstract contract KittyInterface { //zombilerimizi beslemek için CryptoKittieleri kullanalım bunun için kriptokedilerin genlerini okumamız gerekir
@@ -19,22 +18,41 @@ abstract contract KittyInterface { //zombilerimizi beslemek için CryptoKittiele
 }
 
 contract ZombieFeeding is ZombieFactory{
+
     
     
     KittyInterface kittyContract;
 
-    function setKittyContractAddress(address _address) external { //bu sayede eğer CryptoKitties kontratına bir şey olursa bile istediğimiz zaman değiştirebileceğiz ve bunu sadece ownable yapabilecek.
+    modifier ownerOf(uint _zombieId) {
+      require(msg.sender == zombieToOwner[_zombieId]); //değiştirmek için bu zombiye sahip olunması gerekiyor
+      _;
+    }
+
+    function setKittyContractAddress(address _address) external onlyOwner{ //bu sayede eğer CryptoKitties kontratına bir şey olursa bile istediğimiz zaman değiştirebileceğiz şimdilik herks değiştirebiliyor sonraki güncellemde sadece owner değiştirebilecek. 
       kittyContract = KittyInterface(_address);}
     
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
-        require (msg.sender == zombieToOwner[_zombieId] ); //birinin o zombiyi besleyebilmesi için o zombiye sahip olması lazım.
+    function _triggerCooldown(Zombie storage _zombie) internal {
+        
+        _zombie.readyTime=uint32(block.timestamp + cooldownTime);
+        
+    }
+    
+    function _isReady (Zombie storage _zombie) internal view returns(bool){
+        
+       return(_zombie.readyTime <= block.timestamp);
+    }
+    
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
+        
         Zombie storage myZombie = zombies[_zombieId]; //bu zombinin dnasını almak için bunu kullanmalıyız.
+        require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModule;//hedef dnamızın 16 haneli olması gerekiyor
         uint newDna = (_targetDna + myZombie.dna) / 2; //yeni dnayı bu şekilde bir formülle oluşturabilriz
         if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) { //eğer zombiler CryptoKittieleri yedilerse bunu özelleştirelim ve dnasın son 2 hanesini 99 yapalım
           newDna = newDna - newDna % 100 + 99;
     }
         createZombie("NoName",newDna);
+        _triggerCooldown(myZombie);
     }
     
     function feedOnKitty (uint _zombieId ,uint _kittyId) public{
