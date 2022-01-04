fe73559eb53bef73b1e0475315ab1dbfd25324f3
utkukoca <49529559+utkukoca@users.noreply.github.com>
2022-01-04 23:56:41 +0300

    Update IERC721.sol

--- IERC721.sol
+++ IERC721.sol
@@ -13,4 +13,6 @@ interface IERC721 {
         uint256 tokenId
     ) external;
   function approve(address to, uint256 tokenId) external;
+  function mint(address to, uint256 tokenId) external;
+  function burn(uint256 tokenId) external;
 }
